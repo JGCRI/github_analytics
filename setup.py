3ae16ab068cff68fdc3988a51be5144bc1da6154
crvernon <chrisrvernon@gmail.com>
2020-02-17 17:24:57 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='github_analytics',
+    version='v0.1.0',
+    packages=['github_analytics'],
+    url='https://github.com/JGCRI/github_analytics',
+    license='BSD 2-Clause Simplified',
+    author='Chris R. Vernon',
+    author_email='chris.vernon@pnnl.gov',
+    description='Mine and archive GitHub repositories for insight analytics'
+)
