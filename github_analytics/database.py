5a5d7475c6bacee63597eeda930e31bc22f2749c
crvernon <chrisrvernon@gmail.com>
2020-02-17 17:17:12 -0500

    adding database functions

---
+++ github_analytics/database.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+
+def create_connection(db_file):
+    """Create a database connection to the SQLite database specified by db_file
+
+    :param db_file:       database file
+    :return:              connection object or None
+
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+
+    except:
+        raise
+
+    return conn
+
+
+def insert_row(conn, sql, data_tuple):
+    """Insert row into database using tuple and advance row.
+
+    :param conn:            Database connection object
+    :param sql:             SQL string containing insert statement
+    :param data_tuple:      Tuple with data matching input SQL requirements
+
+    :return:                Last row id
+
+    """
+
+    cur = conn.cursor()
+    cur.execute(sql, data_tuple)
+
+    return cur.lastrowid
+
+
+def update_row(conn, sql, data_tuple):
+    """Update row in database using tuple and commit.
+
+    :param conn:            Database connection object
+    :param sql:             SQL string containing insert statement
+    :param data_tuple:      Tuple with data matching input SQL requirements
+
+    """
+
+    cur = conn.cursor()
+    cur.execute(sql, data_tuple)
+    conn.commit()
