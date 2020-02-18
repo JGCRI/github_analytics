3c3e815d0b43872f8cabab4ac54f7fe422d3e0f2
crvernon <chrisrvernon@gmail.com>
2020-02-18 11:54:44 -0500

    tests for convert_google_time

--- githubstats/tests/test_utils.py
+++ githubstats/tests/test_utils.py
@@ -7,7 +7,7 @@ class TestUtils(unittest.TestCase):
     """Test `utils.py` functions."""
 
     def test_leading_zero(self):
-        """Check the number of months in the working hours file derived list."""
+        """Tests for leading_zero function."""
 
         # check for string length greater than 2
         with self.assertRaises(ValueError):
@@ -22,3 +22,14 @@ class TestUtils(unittest.TestCase):
 
         # ensure no zero insertion on length 2 string
         self.assertEqual(u.leading_zero('11'), '11')
+
+    def test_convert_google_time(self):
+        """Tests for convert_google_time function."""
+
+        expected_input = '2016-10-21T00:00:00Z'
+        expected_output = '2016-10-21 00:00:00'
+
+        out_time = u.convert_google_time(expected_input)
+
+        # ensure converted time is as expected
+        self.assertEqual(out_time, expected_output)
